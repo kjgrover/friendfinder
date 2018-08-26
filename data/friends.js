0576a0f881cde44ad4d54b4a23b0058a959b6ad5
kjgrover <kaje14@bellsouth.net>
2018-08-26 11:37:45 -0600

    cleaned code

--- data/friends.js
+++ data/friends.js
@@ -1,18 +1,4 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
-
-// var tableArray = [
-//   {
-//     customerName: "Ahmed",
-//     customerEmail: "ahmed@example.com",
-//     customerID: "afhaque89",
-//     phoneNumber: "000-000-0000"
-//   }
-// ];
+//Prefilled table for testing purposes
 
 var tableArray =[
   {
@@ -69,8 +55,6 @@ var tableArray =[
 ;
 
 
-
-// Note how we export the array. This makes it accessible to other files using require.
 module.exports = tableArray;
 
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,47 +1,22 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 var tableData = require("../data/friends");
-
-
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
     res.json(tableData);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
+
     // req.body is available since we're using the body-parser middleware
     tableData.push(req.body);
 
     var userScores = tableData[tableData.length-1].scores
-
-
     var userScoreArray = [];
-
     var absArray = [];
 
     for (n=0; n<10; n++){
@@ -61,17 +36,12 @@ module.exports = function(app) {
         sumArray.push(absnum)
       }
 
-      
       var totsum = sumArray.reduce((a, b) => a + b, 0)
       absArray.push(totsum)
-
       var sumArray = [];
-
     }
 
-
     var minposition = 0;
-
     var value = absArray[0]
 
     for (q=0; q<absArray.length; q++){
@@ -82,12 +52,7 @@ module.exports = function(app) {
     }
 
     var chosen = tableData[minposition];
-
-
     res.json(chosen);
-
-
- 
   });
 
 
